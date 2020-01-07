7d2759977cd6be6e3c47275ed0bcd3755228ac8c
minghsuanli <minghsuanli@liontravel.com>
2020-01-07 17:51:25 +0800

    新增 DB欄位 Class物件

---
+++ YongqingProgramTest/Library/DAL/Customer_DataObject.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Library.Interface.DAL;
+using Library.Libray;
+using System.Linq;
+
+namespace Library.DAL
+{
+    //class Customer_DataObject
+    public class Customer_DataObject : ICustomer_DataObject, IDisposable
+    {
+        private DbContext _EF = null;
+        public Customer_DataObject(DbContext EF)
+        {
+            try
+            {
+                if (this._EF == null)
+                {
+                    this._EF = EF;
+                }
+            }
+            catch (Exception)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public List<Customer> GetAll()
+        {
+            try
+            {
+                return _EF.Set<Customer>().ToList();
+            }
+            catch (Exception)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public List<Customer> GetById(string Id)
+        {
+            try
+            {
+                if (Id == "")
+                {
+                    return _EF.Set<Customer>().Take(200).ToList();
+                }
+                return _EF.Set<Customer>().Where(x => x.CustomerId == Id).ToList();
+            }
+            catch (Exception)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Create(Customer item)
+        {
+            try
+            {
+                if (item == null)
+                {
+                    throw new ArgumentNullException("Item");
+                }
+                else
+                {
+                    _EF.Set<Customer>().Add(item);
+                    _EF.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Update(Customer item)
+        {
+            try
+            {
+                if (item == null)
+                {
+                    throw new ArgumentNullException("Item");
+                }
+                else
+                {
+                    _EF.Entry(item).State = EntityState.Modified;
+                    _EF.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Delete(Customer item)
+        {
+            try
+            {
+                if (item == null)
+                {
+                    throw new ArgumentNullException("Item");
+                }
+                else
+                {
+                    _EF.Entry(item).State = EntityState.Deleted;
+                    _EF.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private bool disposed = false;
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _EF.Dispose();
+                }
+                disposed = true;
+            }
+        }
+    }
+}
--- YongqingProgramTest/YongqingProgramTest/Startup.cs
+++ YongqingProgramTest/YongqingProgramTest/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library.EF;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -24,13 +26,17 @@ namespace YongqingProgramTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
+            var connectionString = Configuration.GetConnectionString("SampleDB");
+            services.AddDbContext<NorthwindContext>
+            (
+                options => options.UseSqlServer(connectionString)
+            );
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
